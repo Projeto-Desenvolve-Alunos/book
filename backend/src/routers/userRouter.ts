2f850f5574d65194cd70b4440baefc21ef4d5a9e
Jhamyllie <milli.santana2017@gmail.com>
2025-10-05 01:51:39 -0300

    feat: adicona cadastro de usuario

--- backend/src/routers/userRouter.ts
+++ backend/src/routers/userRouter.ts
@@ -4,15 +4,10 @@ import {
 } from "../controllers/userController.js"
 // console.log("userController:", userController);
 
-
 const router = Router();
-router.use((req, _res, next) => {
-  console.log('➡️ Entrou no userRouter:', req.method, req.originalUrl);
-  next();
-});
-
 
 router.get("/", userController.getAllUsers);
 router.get("/:id", userController.getUserById);
+router.post("/create", userController.addUser)
 
 export default router;
