c29f2a718a5938a4f2643740a7cce76309255cc4
Jhamyllie <milli.santana2017@gmail.com>
2025-10-04 22:50:02 -0300

    feat: adicona funções para cadastrar e obter um livro

--- backend/src/controllers/bookController.ts
+++ backend/src/controllers/bookController.ts
@@ -13,6 +13,68 @@ async function getAllBooks(_req: Request, res: Response): Promise<Response> {
     }
 }
 
+async function getBookById(req: Request, res: Response){
+    try {
+        const { id } = req.params;
+
+        if (!id || typeof id !== "string") {
+            return res.status(400).json({ error: "ID não fornecido" });
+        }
+
+        const book = await BookService.getBookById(id);
+        if(!book){
+            return res.status(404).json({message: "Livro não encontrado"});
+        }
+
+        return res.status(200).json(book);
+    } catch (error) {
+        console.error("Erro no Controller getAllBooks:", error);
+        return res.status(500).json({ error: "Erro interno ao buscar livro" });
+    }
+}
+
+async function addBook(req: Request, res: Response) {
+  try {
+    const {
+      title,
+      author,
+      genre,
+      year,
+      pages,
+      rating,
+      synopsis,
+      cover,
+      usuarioId,
+    } = req.body;
+
+    if (!title || !author || !usuarioId) {
+      return res.status(400).json({ error: "Campos obrigatórios ausentes." });
+    }
+
+    const newBook = await BookService.addBook({
+        title,
+        author,
+        genre: genre || null,
+        year: year || null,
+        pages: pages || null,
+        rating: rating || null,
+        synopsis: synopsis || null,
+        cover: cover || null,
+        usuarioId,
+
+    });
+
+    return res.status(201).json(newBook);
+  } catch (error) {
+    console.error("Erro ao adicionar livro:", error);
+    return res.status(500).json({ error: "Não foi possível adicionar o livro." });
+  }
+}
+
+
+
 export const bookController = {
     getAllBooks,
+    getBookById,
+    addBook
 };
